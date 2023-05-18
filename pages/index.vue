500d60da83bad23570848dd84094bbb1aa8f972d
Cname84 <91961332+Cname84@users.noreply.github.com>
2023-05-18 14:51:07 +1000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,127 @@
 <template>
-  <div class="h-screen flex flex-col bg-slate-100">
-    <div class="flex-1 grid grid-cols-3">
-      <div class="col-span-2 flex">
-        <div class="overflow-y-auto p-8">
-          <app-form-profile
-            v-model:name="data.n"
-            v-model:desc="data.d"
-            v-model:image="data.i"
-          ></app-form-profile>
-          <app-form-hr></app-form-hr>
-          <app-form-social-links
-            v-model:twitter="data.t"
-            v-model:telegram="data.tg"
-            v-model:discord="data.d"
-            v-model:email="data.e"
-            v-model:youtube="data.y"
-          ></app-form-social-links>
-          <app-form-hr></app-form-hr>
-          <app-form-links v-model="data.ls"></app-form-links>
-        </div>
+  <div class="h-screen grid grid-cols-3 divide-x">
+    <div class="col-span-2 h-screen flex flex-col bg-slate-100">
+      <div class="flex-1 overflow-y-auto p-8">
+        <app-form-profile
+          v-model:name="data.n"
+          v-model:desc="data.d"
+          v-model:image="data.i"
+        />
+        <app-form-hr />
+        <app-form-social-links
+          v-model:facebook="data.f"
+          v-model:twitter="data.t"
+          v-model:instagram="data.ig"
+          v-model:github="data.gh"
+          v-model:telegram="data.tg"
+          v-model:linkedin="data.l"
+          v-model:email="data.e"
+          v-model:whatsapp="data.w"
+          v-model:youtube="data.y"
+        />
+        <app-form-hr />
+        <app-form-links v-model="data.ls" />
       </div>
-      <div class="col-span-1 border-t bg-white flex flex-col items-center justify-center">
+      <div class="border-t bg-white flex items-center">
         <button
           @click="prefillDemoData"
-          class="h-12 flex items-center space-x-2 px-4 border-b text-sm font-medium bg-blue-500 text-white hover:bg-blue-600 rounded"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
         >
-          <span>Demonstration</span>
-          <icon name="mdi:code-json" class="h-4 w-4"></icon>
+          <span> Add demo data </span>
+          <icon name="mdi:code-json" class="h-4 w-4" />
         </button>
+        <button
+          @click="publish"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
+        >
         <a
           href="https://t.me/OxSocial"
           target="_blank"
-          class="h-12 flex items-center space-x-2 px-4 border-b text-sm font-medium bg-white text-slate-700 hover:bg-gray-100 rounded"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
         >
           <span>Telegram</span>
-          <icon name="ph:paper-plane-tilt-bold" class="h-4 w-4"></icon>
+          <icon name="ph:paper-plane-tilt-bold" class="h-4 w-4" />
         </a>
       </div>
     </div>
-    <app-form-preview :data="data" class="p-8"></app-form-preview>
+    <app-form-preview :data="data" />
     <a
       href="https://twitter.com/0xsocialETH"
       target="_blank"
       class="absolute bottom-0 right-0 bg-white rounded-tl-lg shadow px-4 py-1 font-medium text-sm text-gray-500"
     >
-      Made by 0xSocial
+      Made by 0xSocial 
     </a>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      data: {
-        n: '',
-        d: '',
-        i: '',
-        t: '',
-        tg: '',
-        discord: '',
-        e: '',
-        y: '',
-        ls: []
-      }
-    };
-  }
+<script setup>
+import { encodeData } from "../utils/transformer";
+const data = ref({
+  n: "",
+  d: "",
+  i: "",
+  f: "",
+  t: "",
+  ig: "",
+  gh: "",
+  tg: "",
+  l: "",
+  e: "",
+  w: "",
+  y: "",
+  ls: [],
+});
+
+const prefillDemoData = () => {
+  data.value = {
+    n: "Elon Musk",
+    d: "I’m Elon Musk, the richest person on earth. Also a pro memer.",
+    i: "https://i.insider.com/56743fad72f2c12a008b6cc0",
+    f: "https://www.facebook.com/john_snow",
+    t: "https://twitter.com/john_snow",
+    ig: "https://www.instagram.com/john_snow",
+    e: "mail@john_snow.cc",
+    gh: "https://github.com/john_snow",
+    tg: "https://t.me/john_snow",
+    w: "+918888888888",
+    y: "https://youtube.com/@john_snow",
+    l: "https://linkedin.com/john_snow",
+    ls: [
+      {
+        l: "My Website",
+        i: "ph:globe-duotone",
+        u: "https://example.com",
+      },
+      {
+        l: "Amazon wishlist",
+        i: "ant-design:amazon-outlined",
+        u: "https://amazon.in",
+      },
+      {
+        l: "React JS course",
+        i: "grommet-icons:reactjs",
+        u: "https://reactjs.org/",
+      },
+      {
+        l: "Donate for our cause",
+        i: "iconoir:donate",
+        u: "https://who.int",
+      },
+      {
+        l: "Download my resume",
+        i: "ph:file-pdf",
+        u: "https://google.com",
+      },
+    ],
+  };
+};
+
+const publish = () => {
+  const url = `${window.location.origin}/1?data=${encodeData(data.value)}`;
+  navigator.clipboard.writeText(url).then(() => {
+    alert("Link copied to clipboard");
+  });
 };
 </script>
-
 
