d019d1877db3f7b2b32796daa194a9d4c5168530
Cname84 <91961332+Cname84@users.noreply.github.com>
2023-05-18 11:20:46 +1000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@
     <div class="col-span-1 h-screen flex flex-col bg-gradient-to-b from-blue-500 to-blue-700 text-white">
       <div class="flex flex-col justify-center h-full px-8">
         <h1 class="text-4xl font-bold mb-4">Welcome to 0xSocial</h1>
-        <p class="text-lg mb-8">Discover the power of our amazing features.</p>
+        <p class="text-lg mb-8">Seamless social profile creations.</p>
         <a
           href="https://example.com"
           target="_blank"
