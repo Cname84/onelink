280cf27e742299ed2a6dd56091aeb866fe3b023f
Cname84 <91961332+Cname84@users.noreply.github.com>
2023-05-18 11:09:00 +1000

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@
           @click="prefillDemoData"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
         >
-          <span> Add demo data </span>
+          <span> Demonstration </span>
           <icon name="mdi:code-json" class="h-4 w-4" />
         </button>
         <button
@@ -46,7 +46,7 @@
     </div>
     <div class="col-span-1 h-screen flex flex-col bg-gradient-to-b from-blue-500 to-blue-700 text-white">
       <div class="flex flex-col justify-center h-full px-8">
-        <h1 class="text-4xl font-bold mb-4">Welcome to My App</h1>
+        <h1 class="text-4xl font-bold mb-4">Welcome to 0xSocial</h1>
         <p class="text-lg mb-8">Discover the power of our amazing features.</p>
         <a
           href="https://example.com"
