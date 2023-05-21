4c3e76d51459d40ff9e533169be853fe0712a3d2
Cname84 <91961332+Cname84@users.noreply.github.com>
2023-05-21 19:40:31 +1000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,10 @@
           <!-- Rest of the code -->
 
         </div>
-        <div class="border-t bg-white flex items-center justify-between">
-          <!-- Buttons at the top of the screen -->
-          <div class="top-buttons">
-            <button @click="prefillDemoData" class="button">Add demo data</button>
-            <button v-if="clipboardSupported" @click="publish" class="button">Publish</button>
-            <a href="https://t.me/OxSocial" target="_blank" class="button">Telegram</a>
-          </div>
+        <div class="border-t bg-white flex items-center justify-between top-buttons">
+          <button @click="prefillDemoData" class="button">Add demo data</button>
+          <button v-if="clipboardSupported" @click="publish" class="button">Publish</button>
+          <a href="https://t.me/OxSocial" target="_blank" class="button">Telegram</a>
         </div>
       </div>
       <app-form-preview :data="data" />
@@ -32,7 +29,89 @@
 </template>
 
 <script setup>
-// Rest of the code remains the same
+import { encodeData } from "../utils/transformer";
+import { ref } from "vue";
+
+const data = ref({
+  n: "",
+  d: "",
+  i: "",
+  f: "",
+  t: "",
+  ig: "",
+  gh: "",
+  tg: "",
+  l: "",
+  e: "",
+  w: "",
+  y: "",
+  ls: [],
+});
+
+const prefillDemoData = () => {
+  data.value = {
+    n: "Elon Musk",
+    d: "I’m Elon Musk, the richest person on earth. Also a pro memer.",
+    i: "https://c.ndtvimg.com/2023-01/e3p66s1o_elon-musk-afp-650_650x400_26_January_23.jpg?im=Resize=(1230,900)",
+    f: "https://www.facebook.com/john_snow",
+    t: "https://twitter.com/john_snow",
+    ig: "https://www.instagram.com/john_snow",
+    e: "mail@john_snow.cc",
+    gh: "https://github.com/john_snow",
+    tg: "https://t.me/john_snow",
+    w: "+918888888888",
+    y: "https://youtube.com/@john_snow",
+    l: "https://linkedin.com/john_snow",
+    ls: [
+      {
+        l: "My Website",
+        i: "ph:globe-duotone",
+        u: "https://example.com",
+      },
+      {
+        l: "Amazon wishlist",
+        i: "ant-design:amazon-outlined",
+        u: "https://amazon.in",
+      },
+      {
+        l: "React JS course",
+        i: "grommet-icons:reactjs",
+        u: "https://reactjs.org/",
+      },
+      {
+        l: "Donate for our cause",
+        i: "iconoir:donate",
+        u: "https://who.int",
+      },
+      {
+        l: "Download my resume",
+        i: "ph:file-pdf",
+        u: "https://google.com",
+      },
+    ],
+  };
+};
+
+const clipboardSupported = "navigator" in globalThis && "clipboard" in navigator;
+
+const publish = () => {
+  const url = `${location.protocol}//${location.host}/1?data=${encodeData(data.value)}`;
+  if (clipboardSupported) {
+    navigator.clipboard.writeText(url).then(() => {
+      alert("Link copied to clipboard");
+    });
+  }
+};
+
+const scrollToContent = () => {
+  const contentElement = document.querySelector(".grid");
+  if (contentElement) {
+    window.scrollTo({
+      top: contentElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+};
 </script>
 
 <style scoped>
@@ -46,37 +125,46 @@
 }
 
 .hero {
-  background-color: #f2f2f2;
+  background-color: #f3f4f6;
   padding: 2rem;
-  margin-bottom: 2rem;
   text-align: center;
 }
 
 .hero-title {
   font-size: 2rem;
   font-weight: bold;
-  color: #333;
-  margin-bottom: 0.5rem;
+  margin-bottom: 1rem;
 }
 
 .hero-subtitle {
   font-size: 1.25rem;
-  color: #666;
+  color: #6b7280;
 }
 
 .scroll-button {
-  background-color: #374151;
+  margin-top: 2rem;
+  background-color: #4f46e5;
   color: #fff;
   border: none;
-  padding: 0.75rem 1rem;
+  padding: 1rem 2rem;
   font-size: 1rem;
-  font-weight: 500;
+  font-weight: bold;
   cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.scroll-button:hover {
+  background-color: #4338ca;
 }
 
 .grid {
   gap: 1rem;
   flex-grow: 1;
+  margin-top: 2rem;
+}
+
+.top-buttons {
+  margin-top: 2rem;
 }
 
 .button {
@@ -93,11 +181,6 @@
   cursor: pointer;
 }
 
-.top-buttons {
-  display: flex;
-  gap: 1rem;
-}
-
 .attribution {
   margin-top: auto;
   background-color: #fff;
@@ -110,4 +193,5 @@
   text-decoration: none;
 }
 </style>
+
 
